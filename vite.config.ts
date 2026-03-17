c0727b205462c363d92b382c9f13306c8d9ad692
labu4bd <labu4bd@gmail.com>
2026-03-17 14:13:59 +0100

    add base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/terra-sentinel/',
+    base: './terra-sentinel',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
